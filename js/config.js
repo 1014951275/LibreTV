c06ccd2a5f6aa30b7b97fcf0b918988ce19640d5
1014951275 <90028761+1014951275@users.noreply.github.com>
2025-05-23 14:43:51 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -109,62 +109,7 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
-    ckzy: {
-    //api: 'https://www.ckzy1.com',
-    api: 'https://ckzy.me/api.php/provide/vod/?ac=list',
-    name: 'CK资源',
-    adult: true
-},
-jkun: {
-    api: 'https://jkunzyapi.com',
-    name: 'jkun资源',
-    adult: true
-},
-bwzy: {
-    api: 'https://api.bwzym3u8.com',
-    name: '百万资源',
-    adult: true
-},
-souav: {
-    api: 'https://api.souavzy.vip',
-    name: 'souav资源',
-    adult: true
-},
-r155: {
-    api: 'https://155api.com',
-    name: '155资源',
-    adult: true
-},
-lsb: {
-    api: 'https://apilsbzy1.com',
-    name: 'lsb资源',
-    adult: true
-},
-huangcang: {
-    api: 'https://hsckzy.vip',
-    name: '黄色仓库',
-    adult: true,
-    detail: 'https://hsckzy.vip'
-},
-yutu: {
-    api: 'https://yutuzy10.com',
-    name: '玉兔资源',
-    adult: true
-},
 
-    // 下面是资源失效率高的API源，不建议使用
-    // subo: {
-    //     api: 'https://subocaiji.com/api.php/provide/vod',
-    //     name: '速播资源'
-    // },
-    // fczy: {
-    //     api: 'https://api.fczy888.me/api.php/provide/vod',
-    //     name: '蜂巢资源'
-    // },
-    // ukzy: {
-    //     api: 'https://api.ukuapi88.com/api.php/provide/vod',
-    //     name: 'U酷资源'
-    // },
 };
 
 // 添加聚合搜索的配置选项
@@ -203,6 +148,7 @@ const M3U8_PATTERN = /\$https?:\/\/[^"'\s]+?\.m3u8/g;
 
 // 添加自定义播放器URL
 const CUSTOM_PLAYER_URL = 'player.html'; // 使用相对路径引用本地player.html
+
 
 // 增加视频播放相关配置
 const PLAYER_CONFIG = {
